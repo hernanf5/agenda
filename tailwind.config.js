3fd520db523a6bb31bc9a93e70a402ef1e11307e
Hernan Funes <funes.hernan.max@gmail.com>
2025-01-17 18:50:44 -0300

    avances en la pagina main

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,21 @@ module.exports =
     {
         extend:
         {
-
+                keyframes: {
+                    slidein: {
+                        from: {
+                            opacity: "0",
+                            transform: "translateY(-10px)",
+                        },
+                        to: {
+                            opacity: "1",
+                            transform: "translateY(0)",
+                        },
+                    },
+                },
+                animation: {
+                    slidein: "slidein 1s ease var(--slidein-delay, 0) forwards",
+                },
         },
     },
     plugins:
